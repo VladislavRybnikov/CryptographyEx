3ffead7670792c98b81fc91edc8986344b664479
Vladyslav Rybnikov <vladyslav_rybnikov@epam.com>
2018-11-07 17:41:49 +0200

    Added app instance handling.

--- CryptographyEx.WinFormsUI/Program.cs
+++ CryptographyEx.WinFormsUI/Program.cs
@@ -14,9 +14,20 @@ namespace CryptographyEx.WinFormsUI
         [STAThread]
         static void Main()
         {
+            bool result;
+            var mutex = new System.Threading.Mutex(true, "UniqueAppId", out result);
+
+            if (!result)
+            {
+                MessageBox.Show("Another instance is already running.");
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+
+            GC.KeepAlive(mutex);
         }
     }
 }
